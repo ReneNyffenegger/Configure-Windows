1ae7fddbdd6b0be2ab8038bfbb1a680fa1ea0822
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-08-11 13:56:23 +0200

    + Comment

--- environment.variables.bat
+++ environment.variables.bat
@@ -1,4 +1,8 @@
 @rem  PATH  / TODO: should probably not be run as administrator.
+@rem
+@rem  Sets Values for environment variables in (as it seems)
+@rem  HKCU\Environment
+@rem
 @rem  ---------------------------
 @setx PATH        "c:\vim\vim73;%PATH%"
 @setx PATH        "c:\scripte;%PATH%"
