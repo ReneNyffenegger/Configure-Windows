c2878378a263bf4ff6b2a389410ae324acc5f405
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-03-25 10:42:42 +0100

    Various changes

--- explorer.bat
+++ explorer.bat
@@ -1,18 +1,27 @@
 @rem Disable autoplay
 @rem ----------------
-@reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" /t REG_DWORD /v DisableAutoplay /d 1 /f
+@reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" /v DisableAutoplay     /t REG_DWORD /d 1 /f
 
 @rem Show file extensions
 @rem --------------------
-@reg add  "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /t REG_DWORD /v HideFileExt /d 0 /f
+@reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"         /v HideFileExt          /t REG_DWORD  /d 0 /f
+                                                                                                                               
+@rem Show hidden files (1: Show, 2: Don't show)                                                                                
+@rem ------------------------------------------                                                                                
+@reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"         /v Hidden               /t REG_DWORD  /d 1 /f
+                                                                                                                               
+@rem Really, show ALL hidden Files                                                                                             
+@rem -----------------------------------                                                                                       
+@reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"         /v SuperHidden          /t REG_DWORD  /d 0 /f 
+@reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"         /v ShowSuperHidde       /t REG_DWORD  /d 1 /f 
 
-@rem Show hidden files (1: Show, 2: Don't show)
-@rem ------------------------------------------
-@reg add  "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /t REG_DWORD /v Hidden /d 1 /f
+@rem Don't change case in file names
+@rem Commented as it seems to be ok in Windows 7 anyway.
+@rem @reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"    /v DontPrettyPath       /t REG_DWORD  /d 1 /f
 
-@rem Really, show ALL hidden Files
-@rem -----------------------------------
-@reg add  "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /t REG_DWORD /v SuperHidden     /d 0 /f 
-@reg add  "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /t REG_DWORD /v ShowSuperHidden /d 1 /f 
+@rem Don't ask to search the internet for the correct program when opening a file with an unknown extension
+@rem ------------------------------------------------------------------------------------------------------
+@reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"         /v NoInternetOpenWith   /t REG_DWORD  /d 1 /f
 
-
+@rem What difference does this value make?
+@reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"         /v NoSimpleStartMenu    /t REG_DWORD  /d 1 /f
