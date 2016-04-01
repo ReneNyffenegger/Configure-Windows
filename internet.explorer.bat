b566fd28dfb22ae836f10a708971ee55906c51ce
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-04-01 07:09:06 +0200

    Turning off spell checking in internet explorer without admin rights, too

--- internet.explorer.bat
+++ internet.explorer.bat
@@ -1,3 +1,5 @@
 @rem Turn off spell checking in internet explorer and applications hosting the web browser control (WebOC).
 @rem Add name of application under key FEATURE_SPELLCHECKING.
-@reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Intenet Explorer\MAIN\FeatureControl\FEATURE_SPELLCHECKING" /v iexplore.exe /t REG_DWORD /d 0 /f
+@rem
+@rem @reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Intenet Explorer\MAIN\FeatureControl\FEATURE_SPELLCHECKING" /v iexplore.exe /t REG_DWORD /d 0 /f
+     @reg add  "HKEY_CURRENT_USER\Software\Microsoft\Intenet Explorer\MAIN\FeatureControl\FEATURE_SPELLCHECKING" /v iexplore.exe /t REG_DWORD /d 0 /f
