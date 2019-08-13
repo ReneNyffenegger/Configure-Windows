f78ce1f1ae704908603a75212faee9e6dfa51ab9
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-08-13 22:34:44 +0200

    Update misc.bat

--- misc.bat
+++ misc.bat
@@ -1,7 +1,3 @@
 @rem Time in milliseconds it takes to open a cascading menu in the start menu
 @rem ------------------------------------------------------------------------
 @reg add "HKEY_CURRENT_USER\Control Panel\Desktop"   /v MenuShowDelay /t REG_DWORD /d 50 /f
-
-@rem Don't reboot while someone is logged in
-@rem ---------------------------------------
-@reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoRebootWithLoggedOnUsers /t REG_DWORD /d 1 /f
